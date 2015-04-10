5a613e6f47663965a1d24583a848dd5a22e16232
Justin Werre <werrejustin@gmail.com>
2015-04-09 20:45:20 -0600

    got arithmetic encoder to work

--- src/encoder.cpp
+++ src/encoder.cpp
@@ -12,49 +12,38 @@
 #include <stdexcept>
 #include <string>
 #include <vector>
+#include "../headers/compressor.h"
 #include "../headers/encoder.h"
+#include "../headers/modelA.h"
 
 void encode(Wavelet &image, std::string file_name)
 {
 	std::ofstream fout;
-	fout.open( file_name.c_str() );
+	std::string temp_file_name = "temp.txt";
+	fout.open( temp_file_name.c_str() );
 
-	for( unsigned int i = 0; i < image.size(); i++ ){
-		int min = 255;
-		int max = -255;
-
-		// find the min and max
-		for( unsigned int j = 0; j < image.at( i ).size(); j++){
-			min = std::min( min, static_cast<int>( image.at( i ).at( j ) ) );
-			max = std::max( max, static_cast<int>( image.at( i ).at( j ) ) );
-		}
-
-		int value_range = max - min + 1;
-		std::vector<int> frequency( value_range, 10 );
-		int total = value_range * 10;
-		double lower_bound = 0;
-		double upper_bound = 1;
-
-		fout << min << " " << max << " ";
-
-		// encode the vector
+	// write the wavelets out to a temporary file
+	for( unsigned int i = 0; i < image.size(); i++ )
+	{
 		for( unsigned int j = 0; j < image.at( i ).size(); j++ )
 		{
-			int current_symbol = static_cast<int>( image.at( i ).at( j ) );
-			int probability = sum( frequency, current_symbol + labs( min ) );
-			double range = upper_bound - lower_bound;
-
-			// compute the new lower and upper bound
-			lower_bound = lower_bound + ( range * probability / total );
-			upper_bound = lower_bound + ( range * frequency.at( current_symbol + labs( min ) ) / total );
-
-			foo( lower_bound, upper_bound, fout );
-
+			fout << image.at( i ).at( j ) << " ";
 		}
 		fout << std::endl;
 	}
 
 	fout.close();
+
+	// compress the file
+    std::ifstream input( temp_file_name.c_str() , std::ifstream::binary );
+    std::ofstream output( file_name.c_str(), std::ofstream::binary );
+    modelA<int, 16, 14> cmodel;
+    cmodel.dump("cmodel", std::clog);
+    compress(input, output, cmodel);
+
+    input.close();
+    output.close();
+	std::remove( temp_file_name.c_str() );
 }
 
 int sum( std::vector<int> frequency, int value )
